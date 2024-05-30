0da5abf987d05bde718988d4124c372f79c69b96
LUANA SANTOS LIMA <58047082+LordByron795@users.noreply.github.com>
2024-05-30 09:58:44 -0300

    Add files via upload

---
+++ DllGraph/DllGraph/GraphHelper.cs
@@ -0,0 +1,81 @@
+﻿using Azure.Identity;
+using Microsoft.Graph.Models.ODataErrors;
+using Microsoft.Graph.Models;
+using Microsoft.Graph;
+using System.Runtime.CompilerServices;
+
+
+namespace DllGraph
+{
+    public class GraphHelper
+    {
+        public static async Task<MessageCollectionResponse?> GetInboxAsync(string TenantId, string ClientId, string ClientSecret, string EmailAdress)
+        {
+                var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+                var clientSecretCredential = new ClientSecretCredential(
+                    TenantId, ClientId, ClientSecret);
+
+                var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+
+                var result = await graphClient.Users[EmailAdress].MailFolders["Inbox"].Messages.GetAsync((config) =>
+                {
+                    // Only request specific properties
+                    config.QueryParameters.Select = new[] { "from", "receivedDateTime", "subject", "sender" };
+
+                    // Get at most 25 results
+                    config.QueryParameters.Top = 25;
+                    // Sort by received time, newest first
+                    config.QueryParameters.Orderby = new[] { "receivedDateTime DESC" };
+                });
+
+                return result;
+
+        }
+
+        public static async Task<MessageCollectionResponse?> GetAttachmentsAsync(string TenantId, string ClientId, string ClientSecret, string EmailAdress)
+        {
+
+                var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+                var clientSecretCredential = new ClientSecretCredential(
+                    TenantId, ClientId, ClientSecret);
+
+                var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+
+                var result = await graphClient.Users[EmailAdress].MailFolders["Inbox"].Messages.GetAsync((config) =>
+                {
+                    // Only request specific properties
+                    config.QueryParameters.Select = new[] { "HasAttachments" };
+
+                    config.QueryParameters.Expand = new[] { "Attachments" };
+
+                });
+
+                return result;
+           
+
+        }
+
+        public static async Task DeleteInboxByIdAsync(string idMessage, string TenantId, string ClientId, string ClientSecret, string EmailAdress)
+        {
+                var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+                var clientSecretCredential = new ClientSecretCredential(
+                    TenantId, ClientId, ClientSecret);
+
+                var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+
+
+                await graphClient.Users[EmailAdress].MailFolders["Inbox"].Messages[idMessage].DeleteAsync();
+
+                // return await result.;        
+
+        }
+
+
+    }
+}
